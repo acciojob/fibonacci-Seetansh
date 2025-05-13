022a90aa8446a0006a9d61f819b360cf1248c5a0
Seetansh <87255041+Seetansh@users.noreply.github.com>
2025-05-13 13:13:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-// your code here
+	let n1 = 0;
+	let n2 = 1;
+	let n3 = 0;
+	for(let i=3; i<=num; i++){	
+		n3 = n1+n2;
+		n1 = n2;
+		n2 = n3;
+	}
+	return n3;
 }
 
 module.exports = fibonacci;
